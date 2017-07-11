b80411e0dbee42f7748c05ea2a1892f07fa8a2d5
Patryk Bakłaj <patrykbaklaj@gmail.com>
2017-07-11 23:22:03 +0200

    Arrow handler added

--- js/app.js
+++ js/app.js
@@ -4,8 +4,6 @@ $(document).ready(function() {
 
   // text to animate (firstName and surName)
   let name = $('h2.text_toAnimate');
-  // set display: block for text, which will be animated
-  name.css("display", "block");
   // text to type
   let h3 = $("h3");
   // animated arrow
@@ -17,6 +15,7 @@ $(document).ready(function() {
   spliter(name, "name_animate");
   // type text function invoke
   typeText(h3);
+
   // arrow Handler
   arrowHandler();
 
@@ -34,6 +33,8 @@ $(document).ready(function() {
 
 
   function spliter(toSplit, spanClass) {
+    // set display: block for text, which will be animated
+    name.css("display", "block");
     let splitedText = toSplit.text().split("");
     toSplit.text("");
     for (let i = 0; i < splitedText.length; i++) {
@@ -45,16 +46,25 @@ $(document).ready(function() {
   function typeText(textToType){
     textToType.typeIt({
       startDelay: 3000,
-      speed: 95,
+      speed: 90,
       lifeLike: true,
       autoStart: false
     });
   }
 
+
   function arrowHandler(){
     let timeoutID = setTimeout(function () {
       arrow.css("display", "block");
     }, 11000);
-  }
+    // scroll effect
+    arrow.on("click", function(){
 
-});
+      $('html,body').animate({
+        scrollTop: $("#about").offset().top},
+        1000);
+        $(this).remove();
+      });
+    }
+
+  });
